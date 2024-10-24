14d8eacc7af3e44f96eed6f26de6803973b31b78
Firewolf1337 <18141890+Firewolf1337@users.noreply.github.com>
2024-10-24 09:03:32 +0200

    added prereq upgrade

--- prereq.py
+++ prereq.py
@@ -1,8 +1,8 @@
 import subprocess
 import sys
 
-subprocess.check_call([sys.executable, "-m", "pip", "install", "pytubefix"])
-subprocess.check_call([sys.executable, "-m", "pip", "install", "musicbrainzngs"])
-subprocess.check_call([sys.executable, "-m", "pip", "install", "ytmusicapi"])
-subprocess.check_call([sys.executable, "-m", "pip", "install", "flask"])
-subprocess.check_call([sys.executable, "-m", "pip", "install", "flask_socketio"])
+subprocess.check_call([sys.executable, "-m", "pip", "install", "--upgrade", "pytubefix"])
+subprocess.check_call([sys.executable, "-m", "pip", "install", "--upgrade", "musicbrainzngs"])
+subprocess.check_call([sys.executable, "-m", "pip", "install", "--upgrade", "ytmusicapi"])
+subprocess.check_call([sys.executable, "-m", "pip", "install", "--upgrade", "flask"])
+subprocess.check_call([sys.executable, "-m", "pip", "install", "--upgrade", "flask_socketio"])
